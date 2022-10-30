241e121db06ff6c3a7a87d8619baad7e3523de2e
Igor Penkin <ipenkin86@gmail.com>
2022-10-30 14:19:35 +0300

    add UserService

--- src/main/java/com/messenger/messengerapp/service/UserService.java
+++ src/main/java/com/messenger/messengerapp/service/UserService.java
@@ -3,7 +3,12 @@ package com.messenger.messengerapp.service;
 import com.messenger.messengerapp.user.Role;
 import com.messenger.messengerapp.user.User;
 
+import java.util.List;
+
 public interface UserService {
-    User saveUser (User user);
-    Role saveRole (Role role);
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
 }
