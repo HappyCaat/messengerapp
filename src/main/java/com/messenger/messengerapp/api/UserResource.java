97db519d141b3f14c6c3564b380471b804ea09b6
Igor Penkin <ipenkin86@gmail.com>
2022-11-01 20:02:56 +0300

    edit application.properties

--- src/main/java/com/messenger/messengerapp/api/UserResource.java
+++ src/main/java/com/messenger/messengerapp/api/UserResource.java
@@ -4,6 +4,7 @@ import com.messenger.messengerapp.service.UserService;
 import com.messenger.messengerapp.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.List;
 public class UserResource {
     private final UserService userService;
 
+    @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
