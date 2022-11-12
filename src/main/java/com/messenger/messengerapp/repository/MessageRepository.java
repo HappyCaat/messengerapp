228943734941a3c7c1d12ce8a551aae9f70118c5
Igor Penkin <ipenkin86@gmail.com>
2022-11-12 15:23:14 +0300

    new start

--- src/main/java/com/messenger/messengerapp/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.messenger.messengerapp.repository;
-
-import com.messenger.messengerapp.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
